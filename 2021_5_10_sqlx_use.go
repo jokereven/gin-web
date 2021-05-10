f67465e02f7b5843f1dd938caa7a5fe62d38b5fc
jokereven125 <zhou125disorder@163.com>
2021-05-10 20:39:41 +0800

    sqlx_use

--- 2021_5_10_sqlx_use.go
+++ 2021_5_10_sqlx_use.go
@@ -23,10 +23,98 @@ func initDBsqlxuse() (err error) {
 	return
 }
 
+type usersqlx struct {
+	ID   int    `db:"id"` //首字母大写对外 映射到数据库里面的东西 `db:"xxx"`
+	Name string `db:"name"`
+	Age  int    `db:"age"`
+}
+
+// 查询单条数据示例
+func queryRowDemosqlxuse() {
+	sqlStr := "select id, name, age from user where id=?"
+	var u usersqlx
+	err := dbsqlxuse.Get(&u, sqlStr, 1)
+	if err != nil {
+		fmt.Printf("get failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.ID, u.Name, u.Age)
+}
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	var users []usersqlx
+	err := dbsqlxuse.Select(&users, sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("users:%#v\n", users)
+}
+
+// 插入数据
+func insertRowDemosqlx() {
+	sqlStr := "insert into user(name, age) values (?,?)"
+	ret, err := dbsqlxuse.Exec(sqlStr, "沙河小王子", 19)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入数据的id
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
+}
+
+// 更新数据
+func updateRowDemosqlx() {
+	sqlStr := "update user set age=? where id = ?"
+	ret, err := dbsqlxuse.Exec(sqlStr, 39, 6)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success, affected rows:%d\n", n)
+}
+
+// 删除数据
+func deleteRowDemosqlx() {
+	sqlStr := "delete from user where id = ?"
+	ret, err := dbsqlxuse.Exec(sqlStr, 6)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
+}
+
 func main() {
 	if err := initDBsqlxuse(); err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println("连接成功...")
+
+	queryRowDemosqlxuse() //查询单条根据指定的类容
+
+	queryMultiRowDemo() //查询多条 切片类型
+
+	// insertRowDemosqlx() //插入
+
+	// updateRowDemosqlx() //更新
+
+	deleteRowDemosqlx() //删除
 }
