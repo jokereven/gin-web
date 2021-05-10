64b73d22cbf659c3436f42809067aca2a9894e10
jokereven125 <zhou125disorder@163.com>
2021-05-10 12:37:28 +0800

    sqlx_use

--- 2021_5_10_sqlx_use.go
+++ 2021_5_10_sqlx_use.go
@@ -8,23 +8,23 @@ import (
 	_ "github.com/go-sql-driver/mysql" //init()匿名导入
 )
 
-var dbsqlx *sqlx.DB
+var dbsqlxuse *sqlx.DB
 
-func initDBsqlx() (err error) {
+func initDBsqlxuse() (err error) {
 	dsn := "root:3144588210XZQxzq@tcp(127.0.0.1:3306)/go_dome?charset=utf8mb4&parseTime=True"
 	// 也可以使用MustConnect连接不成功就panic
-	dbsqlx, err = sqlx.Connect("mysql", dsn)
+	dbsqlxuse, err = sqlx.Connect("mysql", dsn)
 	if err != nil {
 		fmt.Printf("connect DB failed, err:%v\n", err)
 		return
 	}
-	dbsqlx.SetMaxOpenConns(20)
-	dbsqlx.SetMaxIdleConns(10)
+	dbsqlxuse.SetMaxOpenConns(20)
+	dbsqlxuse.SetMaxIdleConns(10)
 	return
 }
 
 func main() {
-	if err := initDBsqlx(); err != nil {
+	if err := initDBsqlxuse(); err != nil {
 		fmt.Println(err)
 		return
 	}
